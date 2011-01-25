8a30617904e7fa4da21b9334ec7f1efcdc6879dd
Iain Watt <wattid@mac.com>
2011-01-25 22:13:29 +0000

    Added a User factory and tests for the show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                   "Iain Watt"
+  user.email                  "wattid@example.com"
+  user.password               "foobar"
+  user.password_confirmation  "foobar"
+end
